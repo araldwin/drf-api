029f3193802c6ecda68518a79b82c49a63fda598
araldwin <araldwin@gmail.com>
2023-07-08 19:26:54 +0000

    correct typos, add comments filter

--- profiles/views.py
+++ profiles/views.py
@@ -1,6 +1,6 @@
 from django.db.models import Count
 from rest_framework import generics, filters
-from django_filters.rest_framework import DjanoFilterBackend
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Profile
 from .serializers import ProfileSerializer
@@ -19,12 +19,11 @@ class ProfileList(generics.ListAPIView):
     serializer_class = ProfileSerializer
     filter_backends = [
         filters.OrderingFilter,
-        DjangoFilterBackEnd,
+        DjangoFilterBackend,
     ]
     filterset_fields = [
-        'owner__following__followed_profile',
-        'owner__followed__owner__profile'
-        
+        'owner__following__followed__profile',
+        'owner__followed__owner__profile',
     ]
     
     ordering_fields = [
