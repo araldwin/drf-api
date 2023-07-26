0670d5643b441b073247571ff60ba4f0d9d4bd56
araldwin <araldwin@gmail.com>
2023-07-26 12:26:14 +0000

    deployment

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -61,7 +61,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'DEV' in os.environ
 
-ALLOWED_HOSTS = [
+ALLOWED_HOST = [
     os.environ.get('ALLOWED_HOST'),
     'localhost',
 ]
