1b988e628a313f661579e5f793c3d2ef24ad61e0
araldwin <araldwin@gmail.com>
2023-07-27 06:56:37 +0000

    fixed

--- posts/views.py
+++ posts/views.py
@@ -14,8 +14,8 @@ class PostList(generics.ListCreateAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticatedOrReadOnly]
     queryset = Post.objects.annotate(
-        comments_count=Count('comment', distinct=True),
-        likes_count=Count('likes', distinct=True)
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
     filter_backends = [
         filters.OrderingFilter,
@@ -32,10 +32,11 @@ class PostList(generics.ListCreateAPIView):
         'title',
     ]
     ordering_fields = [
-        'comments_count',
         'likes_count',
-        'likes__created_at'
+        'comments_count',
+        'likes__created_at',
     ]
+
     def perform_create(self, serializer):
         serializer.save(owner=self.request.user)
 
@@ -47,6 +48,6 @@ class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializer
     permission_classes = [IsOwnerOrReadOnly]
     queryset = Post.objects.annotate(
-        comments_count=Count('comment', distinct=True),
-        likes_count=Count('likes', distinct=True)
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
     ).order_by('-created_at')
